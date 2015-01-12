e88303f4cca38e509f1397fbf6b918209176b06f
megfault <ana@megfau.lt>
2015-01-12 01:03:15 +0100

    using ugly absolute addresses; forgive me

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-/*
+g/*
 	Alpha by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
@@ -9,13 +9,13 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global:		{ range: '*', href: 'css/style.css', containers: '60em', grid: { gutters: ['2em', 0] } },
-			wide:		{ range: '-1680', href: 'css/style-wide.css' },
-			normal:		{ range: '-1280', href: 'css/style-normal.css', viewport: { scalable: false } },
-			narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '90%' },
-			narrower:	{ range: '-840', href: 'css/style-narrower.css', containers: '90%!', grid: { zoom: 2 } },
-			mobile:		{ range: '-736', href: 'css/style-mobile.css', containers: '100%!' },
-			mobilep:	{ range: '-480', href: 'css/style-mobilep.css', grid: { zoom: 3 } }
+			global:		{ range: '*', href: 'http://freifunk-darmstadt.github.io/css/style.css', containers: '60em', grid: { gutters: ['2em', 0] } },
+			wide:		{ range: '-1680', href: 'http://freifunk-darmstadt.github.io/css/style-wide.css' },
+			normal:		{ range: '-1280', href: 'http://freifunk-darmstadt.github.io/css/style-normal.css', viewport: { scalable: false } },
+			narrow:		{ range: '-980', href: 'http://freifunk-darmstadt.github.io/css/style-narrow.css', containers: '90%' },
+			narrower:	{ range: '-840', href: 'http://freifunk-darmstadt.github.io/css/style-narrower.css', containers: '90%!', grid: { zoom: 2 } },
+			mobile:		{ range: '-736', href: 'http://freifunk-darmstadt.github.io/css/style-mobile.css', containers: '100%!' },
+			mobilep:	{ range: '-480', href: 'http://freifunk-darmstadt.github.io/css/style-mobilep.css', grid: { zoom: 3 } }
 		},
 		plugins: {
 			layers: {
